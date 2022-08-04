fa0a859e2a1139888f8935b19e8f10693ce0ba45
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-04 19:40:43 -0300

    feat: return higher salary offer in the jobs

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs = read(path)
+    result = 0
+    for job in jobs:
+        if job["max_salary"].isdigit() and int(job["max_salary"]) > result:
+            result = int(job["max_salary"])
+    return result
 
 
 def get_min_salary(path):
