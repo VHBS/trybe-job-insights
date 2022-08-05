cfd6b9bf372c3dfe704bd8e8acfcb6047cada960
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-05 01:18:14 -0300

    test: checks if the function returns a list of dictionaries with the english keys

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    list_job = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in list_job:
+        assert job.get("title") is not None
+        assert job.get("salary") is not None
+        assert job.get("type") is not None
