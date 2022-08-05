e2fcfc80f2227cbde12038021601a408f7baee67
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-05 00:27:13 -0300

    test: checking if the function returns the number of words

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    "Retorna o valor correto de quantas vezes javascript foi mencionado"
+    assert count_ocurrences("src/jobs.csv", "Javascript") == 122
+    assert count_ocurrences("src/jobs.csv", "javascript") == 122
+    assert count_ocurrences("src/jobs.csv", "Python") == 1639
+    assert count_ocurrences("src/jobs.csv", "python") == 1639
